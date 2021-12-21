ea4e21ad82fdd5fd347101b4082eda3db9021648
edwardbas-pl <edwardbas2137@gmail.com>
2021-12-21 13:43:53 +0100

    amall changes ty makepkg_flags function definition

--- install.py
+++ install.py
@@ -313,9 +313,9 @@ def makepkg_flags( chroot ):
     nc = subprocess.getoutput ([ "grep -c ^processor /proc/cpuinfo" ])
     print( "you have " + nc + " cores" )
     print( "Changing makeflags for " + nc + " cores" )
-    os.system( chroot + " sed -i 's/#MAKEFLAGS='-j2'/MAKEFLAGS='-j" + nc + "/g' /mnt/etc/makepkg.conf" )
+    os.system( "sed -i 's/#MAKEFLAGS='-j2'/MAKEFLAGS='-j" + nc + "/g' /mnt/etc/makepkg.conf" )
     print( "Changing the compression settings for " + nc + " cores" )
-    os.system( chroot + " sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T " + nc + " -z -)/g' /mnt/etc/makepkg.conf" )
+    os.system( "sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T " + nc + " -z -)/g' /mnt/etc/makepkg.conf" )
 
 def networking( strap , chroot ):
     os.system( strap + " networkmanager" )
@@ -379,5 +379,5 @@ user_setup( CHROOT , username , password )
 set_locale( CHROOT )
 host_settings( hostname )
 bootlooader_determine( STRAP , CHROOT , ROOT , SWAP , install_path )
-makepkg_flags()
+makepkg_flags( CHROOT )
 networkmanager( STRAP , CHROOT ) 
