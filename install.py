1bef9c0d81c946f467f982d84503c42324f91a1b
edwardbas-pl <edwardbas2137@gmail.com>
2021-12-21 13:41:14 +0100

    corrected gtub legacy install function call

--- install.py
+++ install.py
@@ -301,7 +301,7 @@ def bootlooader_determine( strap , chroot , root , swap , path ):
             systemdboot_install( chroot , root , swap )
         else: grub_efi( strap ,chroot , path )
     else:
-        grub_legacy_install()
+        grub_legacy_install( strap , chroot , path )
 
 def cpu_microcodes_install( strap ):
     if cpu_detect() == 'intel':
