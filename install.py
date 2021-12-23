af160d07bbd44d278df7c3d7c415bd402aa838bc
edwardbas-pl <edwardbas2137@gmail.com>
2021-12-23 12:12:17 +0100

    debuging

--- install.py
+++ install.py
@@ -337,8 +337,8 @@ STRAP = ""
 CHROOT = ""
 set_strap_and_chroot()
 
-#if distro_check() == "arch":
-    #mirror_refresh()
+if distro_check() == "arch":
+    mirror_refresh()
 
 #check ho many ram (in MB) in order to build sufficient swap partition
 mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
@@ -406,9 +406,6 @@ else:
 
 print("end of problematic moment. chech")
 input()
-
-
-
 
 makepkg_flags( CHROOT )
 networking( STRAP , CHROOT ) 
