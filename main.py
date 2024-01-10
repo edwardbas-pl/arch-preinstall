79d0ca9155d65b2e4b9e3b76885b071b29360df7
edwardbas-pl <edwardbas2137@gmail.com>
2024-01-10 18:29:08 +0100

    that wasn't a goodd idea 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import multiprocessing
 import sys
 from hardware import *
 from user_settings import *
-from disk_prepare import get_install_destination
+from disk_prepare import disk_prepare
 from base_install import *
 from profiles import *
 
