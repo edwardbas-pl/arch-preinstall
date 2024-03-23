107d1df8d070a63286a898e5dcc5cfa80b09166f
edwardbas-pl <edwardbas2137@gmail.com>
2024-03-23 16:01:09 +0100

    updat

--- profiles/gnome.sh
+++ profiles/gnome.sh
@@ -5,6 +5,7 @@ sudo echo '
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist ' | sudo tee -a /etc/pacman.conf
 
+sudo pacman-key --init
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
